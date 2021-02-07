dc20546e771698054c82083966ddc6ed505546e7
alikahwaji <ali.kahwaji@hotmail.com>
2021-02-08 00:19:20 +1300

    Fix config file

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,8 +30,8 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'sdv503-site',
-  organizationName: 'NMIT',
+  projectName: 'SDV503',
+  organizationName: 'alikahwaji',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
